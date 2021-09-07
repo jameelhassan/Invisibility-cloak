b6ee345f43d2e64b421573973ab39b1b325d9ba2
Jameel Hassan <jhassan.tck14@gmail.com>
2021-09-07 17:29:06 +0530

    minor edits to colour

--- invisibility-cloak.py
+++ invisibility-cloak.py
@@ -11,6 +11,7 @@ background = 0
 for i in range(30):
 	ret, background = cap.read()
 
+width, height = background.shape[0], background.shape[1]
 background = np.flip(background, axis=1)
 
 while True:
@@ -23,26 +24,33 @@ while True:
 	value = (35,35)
 	blurred = cv2.GaussianBlur(hsv, value, 0)
 
-	#Define lower range for colour- here we use blue
-	# Note: red wraps around 180 which will need two ranges as commented
+	#Define lower range for colour
+	# lower_col = np.array([100, 50, 50])
+	# upper_col = np.array([140, 255, 255])
+	# mask1 = cv2.inRange(hsv, lower_col, upper_col)
+	# mask2 = 0
 
-	lower_col = np.array([110, 100, 50])
-	upper_col = np.array([130, 255, 255])
+	#### THIS IS RED
+	# Note: red wraps around 180 which will need two ranges as commented
+	lower_col = np.array([0, 170, 80])
+	upper_col = np.array([10, 255, 255])
 	mask1 = cv2.inRange(hsv, lower_col, upper_col)
 
 	#Define upper range for Colour - here we use red
-	# lower_col = np.array([170, 120, 70])
-	# upper_col = np.array([180, 255, 255])
-	# mask2 = cv2.inRange(hsv, lower_col, upper_col)
+	lower_col = np.array([170, 170, 80])
+	upper_col = np.array([180, 255, 255])
+	mask2 = cv2.inRange(hsv, lower_col, upper_col)
 
-	mask = mask1 
+	mask = mask1 + mask2 
 	mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, np.ones((5,5), np.uint8))
 
 	#Replace colour pixel with background
 	img[np.where(mask==255)] = background[np.where(mask==255)]
 	cv2.imshow('Display', img)
+
 	k = cv2.waitKey(10)
 	if k == 27:
 		break
+
 
 
